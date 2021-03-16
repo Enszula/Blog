faf8b5fd2aeb4babb97f21e89b0346da3db5f0c0
Enszula <kamil.mensah@outlook.com>
2021-03-16 22:56:56 +0100

    Adding New Post - The Post ModelForm - The new_post URL - The new_post() View Function - The new_post Template

--- blogs/urls.py
+++ blogs/urls.py
@@ -13,5 +13,8 @@ urlpatterns = [
     path('posts/', views.posts, name='posts'),
 
     # Detail page for a single topic.
-    path('posts/<int:post_id>', views.post, name='post')
+    path('posts/<int:post_id>', views.post, name='post'),
+
+    # Page for adding a new topic
+    path('new_post/', views.new_post, name='new_post'),
 ]
