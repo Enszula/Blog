587828b88d05bee8ce85dd6ce1ee6dffda6ab6b6
Enszula <kamil.mensah@outlook.com>
2021-03-16 18:36:40 +0100

    - Writing a View views.py - Writing a Template: index.html - The Parent Template: base.html - The Child Template: rewrite index.html - The Posts Page - The Posts URL Pattern: urls.py - The Posts View: views.py - The post Template: topics.html - modify base.html to include link to the posts page

--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Post
+
+def index(request):
+    """The home page for Blog."""
+    return render(request, 'blogs/index.html')
+
+def posts(request):
+    """Show all topics."""
+    posts = Post.objects.order_by('date_added')
+    context = {'posts': posts}
+    return render(request, 'blogs/posts.html', context)
