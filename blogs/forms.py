5df834b1099e83aae27cd45e9aa917649d4f33fb
Enszula <kamil.mensah@outlook.com>
2021-03-22 00:32:44 +0100

    - remove strip false forms

--- blogs/forms.py
+++ blogs/forms.py
@@ -4,7 +4,7 @@ from .models import Post
 
 class PostForm(forms.ModelForm):
     title = forms.CharField(label="Post Title")
-    text = forms.CharField(label="Text", widget=forms.Textarea(), strip=False)
+    text = forms.CharField(label="Text", widget=forms.Textarea())
     class Meta:
         model = Post
         fields = ['title', 'text']
