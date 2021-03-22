ad3866096bad648dd2d2e55c0571a5a37afe3958
Enszula <kamil.mensah@outlook.com>
2021-03-22 12:47:27 +0100

    - add delete url pattern

--- blogs/urls.py
+++ blogs/urls.py
@@ -12,12 +12,15 @@ urlpatterns = [
     # Page that shows all posts.
     path('posts/', views.posts, name='posts'),
 
-    # Detail page for a single topic.
+    # Detail page for a single post.
     path('posts/<int:post_id>', views.post, name='post'),
 
-    # Page for adding a new topic
+    # Page for adding a new post
     path('new_post/', views.new_post, name='new_post'),
 
-    # Page for editing an entry.
+    # Page for editing an post.
     path('edit_post/<int:post_id>', views.edit_post, name='edit_post'),
+
+    # deleting post
+    path('delete_post/<int:post_id>', views.delete_post, name='delete_post'),
 ]
